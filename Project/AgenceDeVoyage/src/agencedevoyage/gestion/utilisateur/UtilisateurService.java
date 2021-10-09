117938bc1d3e7262159a6151de3954a69daaf973
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-09 22:33:17 +0100

    Create UtilisateurService.java

---
+++ Project/AgenceDeVoyage/src/agencedevoyage/gestion/utilisateur/UtilisateurService.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agencedevoyage.gestion.utilisateur;
+
+import agencedevoyage.IService;
+import java.util.List;
+
+/**
+ *
+ * @author bourg
+ */
+public class UtilisateurService implements IService<Utilisateur>{
+
+    @Override
+    public void ajouter(Utilisateur t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean supprimer(Utilisateur t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modifier(Utilisateur t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Utilisateur> consulter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+    
+}
