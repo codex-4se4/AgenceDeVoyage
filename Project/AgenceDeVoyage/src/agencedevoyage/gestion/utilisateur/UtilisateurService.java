c0e8252104bfde410314cc196df8d61f9ad63fc4
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-11 19:28:40 +0100

    Update UtilisateurService.java

--- Project/AgenceDeVoyage/src/agencedevoyage/gestion/utilisateur/UtilisateurService.java
+++ Project/AgenceDeVoyage/src/agencedevoyage/gestion/utilisateur/UtilisateurService.java
@@ -6,12 +6,20 @@
 package agencedevoyage.gestion.utilisateur;
 
 import agencedevoyage.IService;
+<<<<<<< Updated upstream
+=======
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+>>>>>>> Stashed changes
 import java.util.List;
 
 /**
  *
  * @author bourg
  */
+<<<<<<< Updated upstream
 public class UtilisateurService implements IService<Utilisateur>{
 
     @Override
@@ -34,6 +42,32 @@ public class UtilisateurService implements IService<Utilisateur>{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+=======
+public class UtilisateurService {
+
+   /* @Override
+    public void ajouter(Utilisateur u){
+
+    String query = "INSERT INTO `utilisateur` (`nom`, `prenom`, `login`, `mdp`, `passeport`) VALUES (NULL, '" + t.getNom() + "', '" + t.getPrenom() + "', '" + t.getAge() + "');";
+    executeQuery(query);
+        }
+
+
+
+    @Override
+    public boolean supprimer(Utilisateur u) {
+    }
+
+    @Override
+    public boolean modifier(Utilisateur u){
+    }
+
+    @Override
+    public List<Utilisateur> consulter(){
+
+    }
+*/
+>>>>>>> Stashed changes
 
     
 }
