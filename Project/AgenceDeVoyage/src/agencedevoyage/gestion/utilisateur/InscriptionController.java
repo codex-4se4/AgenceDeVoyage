2be0b46bd5a9893e1fa19261772a3fb910631b72
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-11 22:35:35 +0100

    Update InscriptionController.java

--- Project/AgenceDeVoyage/src/agencedevoyage/gestion/utilisateur/InscriptionController.java
+++ Project/AgenceDeVoyage/src/agencedevoyage/gestion/utilisateur/InscriptionController.java
@@ -5,9 +5,19 @@
  */
 package agencedevoyage.gestion.utilisateur;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +26,71 @@ import javafx.fxml.Initializable;
  */
 public class InscriptionController implements Initializable {
 
+    @FXML
+    private JFXTextField nom;
+    @FXML
+    private JFXTextField prenom;
+    @FXML
+    private JFXTextField email;
+    @FXML
+    private JFXTextField login;
+    @FXML
+    private JFXPasswordField mdp;
+    @FXML
+    private JFXTextField cin;
+    @FXML
+    private JFXTextField passeport;
+    
+    UtilisateurService utilisateurService;
+    @FXML
+    private ImageView progress;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+       progress.setVisible(false);
+       nom.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       prenom.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       email.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       login.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       mdp.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       cin.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       passeport.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       utilisateurService = new UtilisateurService();
+    }
+
+    @FXML
+    private void creerCompteAction(ActionEvent event) {
+        try {
+           Utilisateur user = new Utilisateur(nom.getText(), prenom.getText(), email.getText(), cin.getText(), passeport.getText(), login.getText(), mdp.getText());
+           utilisateurService.ajouter(user); 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        progress.setVisible(true);
+        nom.clear();
+        prenom.clear();
+        email.clear();
+        cin.clear();
+        passeport.clear();
+        login.clear();
+        mdp.clear();
+        
+        
+    }
+
+    @FXML
+    private void authentifierAction(ActionEvent event) throws IOException {
+        login.getScene().getWindow().hide();
+        Stage signIn = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/agencedevoyage/Main.fxml"));
+        Scene scene = new Scene(root);
+        signIn.setScene(scene);
+        signIn.show();
+        signIn.setResizable(false);
+    }
     
 }
