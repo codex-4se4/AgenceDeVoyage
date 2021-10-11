0e7de5a2f81d8f1c0b0d523a1618e03e82ad2d9d
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-11 22:35:48 +0100

    Update MainController.java

--- Project/AgenceDeVoyage/src/agencedevoyage/MainController.java
+++ Project/AgenceDeVoyage/src/agencedevoyage/MainController.java
@@ -5,39 +5,103 @@
  */
 package agencedevoyage;
 
+import agencedevoyage.configuration.database.DBConnection;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
-<<<<<<< Updated upstream
 import javafx.fxml.FXML;
-=======
->>>>>>> Stashed changes
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  *
  * @author bourg
  */
 public class MainController implements Initializable {
-    
-<<<<<<< Updated upstream
+
     @FXML
-    private Label label;
-    
+    private JFXTextField username;
     @FXML
-=======
-    private Label label;
-    
->>>>>>> Stashed changes
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private JFXPasswordField password;
+    @FXML
+    private JFXButton signup;
+    @FXML
+    private JFXButton login;
+    @FXML
+    private JFXCheckBox remember;
+    @FXML
+    private JFXButton forgotpassword;
+    @FXML
+    private ImageView progress;
+    @FXML
+    private Label succes;
+    @FXML
+    private Label echec;
+
+    @FXML
+    private void connexionAction(ActionEvent e) throws SQLException {
+        progress.setVisible(true);
+        String SQL = "SELECT * FROM `utilisateur` WHERE login= "+"'" + username.getText() + "' AND mdp='" + password.getText() +"'";
+        ResultSet rs;
+        
+            rs = loginQuery(SQL);
+            if(rs == null || !rs.next()){
+                
+                echec.setVisible(true);
+                succes.setVisible(false);
+
+                return;
+            }       
+            echec.setVisible(false);
+            succes.setVisible(true);  
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+       progress.setVisible(false);
+       succes.setVisible(false);
+       echec.setVisible(false);
+       username.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+       password.setStyle("-fx-text-inner-color : #a0a2ab;"+"-fx-prompt-text-fill : #a0a2ab;");
+    }
+
+    @FXML
+    private void inscriptionAction(ActionEvent event) throws IOException {
+        login.getScene().getWindow().hide();
+        Stage signup = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/agencedevoyage/gestion/utilisateur/Inscription.fxml"));
+        Scene scene = new Scene(root);
+        signup.setScene(scene);
+        signup.show();
+        signup.setResizable(false);
+    }
     
+     private ResultSet loginQuery(String query) {
+        Connection conn = DBConnection.getInstance().getConnection();
+        Statement st;
+        ResultSet rs;
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+            return null;
+        }
+        return rs;
+    }
+
 }
