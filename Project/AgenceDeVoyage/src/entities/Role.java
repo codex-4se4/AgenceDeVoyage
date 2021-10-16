9184bbedff26e4c03242491a179015c923e96fb9
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-17 00:14:57 +0100

    Create Role.java

---
+++ Project/AgenceDeVoyage/src/entities/Role.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author bourg
+ */
+public class Role {
+    private int id;
+    private String nom;
+
+    public Role() {
+    }
+
+    public Role(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+}
