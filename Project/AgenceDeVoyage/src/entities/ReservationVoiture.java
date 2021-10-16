51d16e1874c4f15b6e4c4170a5e005d25b0e3c9c
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-16 22:03:36 +0100

    Update ReservationVoiture.java

--- Project/AgenceDeVoyage/src/entities/ReservationVoiture.java
+++ Project/AgenceDeVoyage/src/entities/ReservationVoiture.java
@@ -18,5 +18,44 @@ public class ReservationVoiture {
     private Date date_finrv;
     public ReservationVoiture(int idv,float prixv,Date date_debutrv,Date datefinrv){
     }
+
+    public int getIdv() {
+        return idv;
+    }
+
+    public void setIdv(int idv) {
+        this.idv = idv;
+    }
+
+    public float getPrixv() {
+        return prixv;
+    }
+
+    public void setPrixv(float prixv) {
+        this.prixv = prixv;
+    }
+
+    public Date getDate_debutrv() {
+        return date_debutrv;
+    }
+
+    public void setDate_debutrv(Date date_debutrv) {
+        this.date_debutrv = date_debutrv;
+    }
+
+    public Date getDate_finrv() {
+        return date_finrv;
+    }
+
+    public void setDate_finrv(Date date_finrv) {
+        this.date_finrv = date_finrv;
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
