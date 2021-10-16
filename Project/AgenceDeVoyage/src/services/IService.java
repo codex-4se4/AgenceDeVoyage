752acedf887fe9f9dddb9e8cfb8472bc894c5c65
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-16 21:46:16 +0100

    Create IService.java

---
+++ Project/AgenceDeVoyage/src/services/IService.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import database.DBConnection;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author bourg
+ */
+public interface IService<T> {
+
+    void ajouter(T t);
+
+    boolean supprimer(T t);
+
+    boolean modifier(T t);
+
+    ObservableList<T> consulter();
+
+    default void executeQuery(String query) {
+        Connection conn = DBConnection.getInstance().getConnection();
+        Statement st;
+        try {
+            st = conn.createStatement();
+            st.executeUpdate(query);
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+
+}
