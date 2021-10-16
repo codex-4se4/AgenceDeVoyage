b268456be7aaf7edb7e9573c0a4f81d61537a7cb
Frija Jawher <jawher.frija@esprit.tn>
2021-10-16 02:31:44 +0200

    Create Formule.java

---
+++ Project/AgenceDeVoyage/src/agencedevoyage/gestion/hebergement/Formule.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agencedevoyage.gestion.hebergement;
+
+/**
+ *
+ * @author Jawher-Louisa
+ */
+public enum Formule {
+    LogementPetitDejeuner,DemiPension,PensionComplete,AnclusiveComplete,AllInclusiveSoft,AllInclusive
+    
+}
