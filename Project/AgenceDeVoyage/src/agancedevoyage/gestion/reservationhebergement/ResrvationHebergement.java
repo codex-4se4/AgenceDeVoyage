7e81b6df2e55f5b759824eae96d6dc76c7a12a90
fatmaBe <73782362+fatmaBe@users.noreply.github.com>
2021-10-16 03:20:57 +0200

    Update ResrvationHebergement.java

--- Project/AgenceDeVoyage/src/agancedevoyage/gestion/reservationhebergement/ResrvationHebergement.java
+++ Project/AgenceDeVoyage/src/agancedevoyage/gestion/reservationhebergement/ResrvationHebergement.java
@@ -5,10 +5,58 @@
  */
 package agancedevoyage.gestion.reservationhebergement;
 
+import java.date;
+
 /**
  *
  * @author HP
  */
 public class ResrvationHebergement {
+    private int idh;
+    private float prixh;
+    private date date_debutrh;
+    private date date_finrh;
+    public  ResrvationHebergement(int idh,float prixh,date date_debutrh,date datefinrh){
+    this.prixh = prixh;
+    this.date_debutrh = date_debutrh;
+    this.date_finrh = date_finrh;
+    
+}
+
+    public int getIdh() {
+        return idh;
+    }
+
+    public void setIdh(int idh) {
+        this.idh = idh;
+    }
+
+    public float getPrixh() {
+        return prixh;
+    }
+
+    public void setPrixh(float prixh) {
+        this.prixh = prixh;
+    }
+
+    public date getDate_debutrh() {
+        return date_debutrh;
+    }
+
+    public void setDate_debutrh(date date_debutrh) {
+        this.date_debutrh = date_debutrh;
+    }
+
+    public date getDate_finrh() {
+        return date_finrh;
+    }
+
+    public void setDate_finrh(date date_finrh) {
+        this.date_finrh = date_finrh;
+    }
+
+   
+    
+
     
 }
