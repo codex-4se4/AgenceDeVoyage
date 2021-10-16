2ad044de1a0f248986e90c00b20508a7185158ad
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-16 21:46:03 +0100

    Create ReservationVoiture.java

---
+++ Project/AgenceDeVoyage/src/entities/ReservationVoiture.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class ReservationVoiture {
+    private int idv;
+    private float prixv;
+    private Date date_debutrv;
+    private Date date_finrv;
+    public ReservationVoiture(int idv,float prixv,Date date_debutrv,Date datefinrv){
+    }
+    
+}
