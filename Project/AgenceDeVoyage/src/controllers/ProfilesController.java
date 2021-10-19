338b571ca781044a244a75deed2b60fc2ebfe2d9
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-19 20:59:39 +0100

    Create ProfilesController.java

---
+++ Project/AgenceDeVoyage/src/controllers/ProfilesController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import agencedevoyage.AgenceDeVoyage;
+import entities.Utilisateur;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseDragEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author bourg
+ */
+public class ProfilesController implements Initializable {
+
+    @FXML
+    private AnchorPane paneHolder;
+    @FXML
+    private Label nomComplet;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+}
