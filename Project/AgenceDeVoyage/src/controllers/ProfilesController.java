3e787ce88aca06bfc7ad406355162a87c4a6e575
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-19 21:57:31 +0100

    Update ProfilesController.java

--- Project/AgenceDeVoyage/src/controllers/ProfilesController.java
+++ Project/AgenceDeVoyage/src/controllers/ProfilesController.java
@@ -6,17 +6,16 @@
 package controllers;
 
 import agencedevoyage.AgenceDeVoyage;
+import com.jfoenix.controls.JFXButton;
 import entities.Utilisateur;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseDragEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -29,6 +28,32 @@ public class ProfilesController implements Initializable {
     private AnchorPane paneHolder;
     @FXML
     private Label nomComplet;
+    private TextField prenomLabel;
+    private TextField nomLabel;
+    private TextField emailLabel;
+    private TextField passeportLabel;
+    private TextField loginLabel;
+    private TextField cinLabel;
+    @FXML
+    private Label mdpLabel;
+    @FXML
+    private TextField prenom;
+    @FXML
+    private TextField nom;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField passeport;
+    @FXML
+    private TextField login;
+    @FXML
+    private TextField mdp;
+    @FXML
+    private TextField cin;
+    @FXML
+    private JFXButton editer;
+    @FXML
+    private JFXButton sauvegarder;
 
     /**
      * Initializes the controller class.
@@ -38,4 +63,41 @@ public class ProfilesController implements Initializable {
 
     }
 
+    public void setUserInformation(Utilisateur u) {
+        nomComplet.setText(u.getPrenom() + " " + u.getNom());
+        prenom.setText(u.getPrenom());
+        nom.setText(u.getNom());
+        email.setText(u.getEmail());
+        cin.setText(u.getCin());
+        passeport.setText(u.getPasseport());
+        login.setText(u.getLogin());
+    }
+
+    @FXML
+    private void editerAction(ActionEvent event) {
+        prenom.setEditable(true);
+        nom.setEditable(true);
+        email.setEditable(true);
+        cin.setEditable(true);
+        passeport.setEditable(true);
+        login.setEditable(true);
+        editer.setVisible(false);
+        sauvegarder.setVisible(true);
+        mdp.setVisible(true);
+        mdpLabel.setVisible(true);
+    }
+
+    @FXML
+    private void sauvegarderAction(ActionEvent event) {
+        prenom.setEditable(false);
+        nom.setEditable(false);
+        email.setEditable(false);
+        cin.setEditable(false);
+        passeport.setEditable(false);
+        login.setEditable(false);
+        editer.setVisible(true);
+        sauvegarder.setVisible(false);
+        mdp.setVisible(false);
+        mdpLabel.setVisible(false);
+    }
 }
