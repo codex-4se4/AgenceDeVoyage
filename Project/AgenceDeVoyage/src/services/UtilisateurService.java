bc6c16b5f8a2be4a245462696ec881c2bdf894f0
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-10-20 19:28:03 +0100

    Update UtilisateurService.java

--- Project/AgenceDeVoyage/src/services/UtilisateurService.java
+++ Project/AgenceDeVoyage/src/services/UtilisateurService.java
@@ -6,9 +6,7 @@
 package services;
 
 import entities.Utilisateur;
-import services.IService;
 import database.DBConnection;
-import entities.Role;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -19,56 +17,55 @@ import javafx.collections.ObservableList;
  *
  * @author bourg
  */
-public class UtilisateurService implements IService<Utilisateur>{
+public class UtilisateurService implements IService<Utilisateur> {
 
     @Override
     public void ajouter(Utilisateur u) {
-        String SQL = "INSERT INTO `utilisateur` (`nom`, `prenom`, `email`,`cin`,`passeport`,`login`, `mdp`) VALUES ("+"'" + u.getNom() + "', '" + u.getPrenom() + "', '" 
-                + u.getEmail() + "','"+ u.getCin()+"','" + u.getPasseport() + "','" + u.getLogin()+ "','"+ u.getMdp() + "')";
+        String SQL = "INSERT INTO `utilisateur` (`nom`, `prenom`, `email`,`cin`,`passeport`,`login`, `mdp`) VALUES (" + "'" + u.getNom() + "', '" + u.getPrenom() + "', '"
+                + u.getEmail() + "','" + u.getCin() + "','" + u.getPasseport() + "','" + u.getLogin() + "','" + u.getMdp() + "')";
         executeQuery(SQL);
     }
 
     @Override
     public boolean supprimer(Utilisateur u) {
-         String SQL= "DELETE FROM `utilisateur` WHERE id="+u.getId();
-         executeQuery(SQL);
-         return true;
+        String SQL = "DELETE FROM `utilisateur` WHERE id=" + u.getId();
+        executeQuery(SQL);
+        return true;
 
     }
 
     @Override
     public boolean modifier(Utilisateur u) {
-        String SQL ="UPDATE `utilisateur` SET nom ='" +u.getNom()+"',prenom ='"+ u.getPrenom()+
-                "',email ="+ u.getEmail()+"',cin ='"+u.getCin()+"',passeport='"+u.getPasseport()
-                +",login='"+u.getLogin()+"',mdp='"+u.getMdp() +"' WHERE id ="+u.getId();
-        executeQuery(SQL);   
-        return true; }
+        String SQL = "UPDATE `utilisateur` SET nom ='" + u.getNom() + "', prenom ='" + u.getPrenom()
+                + "',email ='" + u.getEmail() + "',cin ='" + u.getCin() + "',passeport='" + u.getPasseport()
+                + "',login='" + u.getLogin() + "',mdp='" + u.getMdp() + "' WHERE id =" + u.getId();
+        executeQuery(SQL);
+        return true;
+    }
 
     @Override
     public ObservableList<Utilisateur> consulter() {
-        ObservableList<Utilisateur> utilisateurs =FXCollections.observableArrayList();
+        ObservableList<Utilisateur> utilisateurs = FXCollections.observableArrayList();
         Connection conn = DBConnection.getInstance().getConnection();
-        String query ="SELECT * FROM `utilisateur`";
+        String query = "SELECT * FROM `utilisateur`";
         Statement st;
         ResultSet rs;
-        try{
-            st =conn.createStatement();
+        try {
+            st = conn.createStatement();
             rs = st.executeQuery(query);
             Utilisateur utilisateur;
-            while(rs.next()){
-                utilisateur =new Utilisateur(rs.getString("nom"),rs.getString("prenom"),rs.getString("email"),
-                rs.getString("cin"),rs.getString("passeport"),rs.getString("login"),rs.getString("mdp"));
+            while (rs.next()) {
+                utilisateur = new Utilisateur(rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),
+                        rs.getString("cin"), rs.getString("passeport"), rs.getString("login"), rs.getString("mdp"));
                 utilisateurs.add(utilisateur);
-                
+
             }
-           
-        }
-        catch(Exception ex){
+
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return utilisateurs;
-         
-         
-         }
-    
+
+    }
+
 }
