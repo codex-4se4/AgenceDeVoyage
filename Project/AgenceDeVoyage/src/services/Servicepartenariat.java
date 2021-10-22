51964d054db12f1c9cfc3b3f8b6734d241ca55de
Wiemifaoui <73914179+wiemifaoui@users.noreply.github.com>
2021-10-22 10:42:32 +0200

    Update Servicepartenariat.java

--- Project/AgenceDeVoyage/src/services/Servicepartenariat.java
+++ Project/AgenceDeVoyage/src/services/Servicepartenariat.java
@@ -5,7 +5,13 @@
  */
 package services;
 
-import entities.partenariat;
+import database.DBConnection;
+import entities.Partenariat;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 
 
@@ -13,31 +19,51 @@ import entities.partenariat;
  *
  * @author HP
  */
-public abstract class Servicepartenariat implements IService <partenariat>{
+public abstract class Servicepartenariat implements IService <Partenariat>{
     @Override
-    public void ajouter(partenariat p){
+    public void ajouter(Partenariat p){
         String SQL = "INSERT INTO `partenariat` (`id`, `nom`, `adresse`,`ContratDebut`,`ContratFin`) VALUES ("+"'" + p.getId() + "', '" + p.getNom() + "', '"
                 + p.getAdresse() + "','"+ p.getContratDebut()+"','" + p.getContratFin() + "')";
         executeQuery(SQL);
     }
        
     @Override
-    public boolean supprimer(partenariat p) {
+    public boolean supprimer(Partenariat p) {
     String SQL = "DELETE FROM 'partenariat' Where ID=" +p.getId() ;
     executeQuery(SQL);
         return true;
     }
-    public boolean modifier(partenariat p) {
+    public boolean modifier(Partenariat p) {
         String SQL = "UPDATE `partenariat`  (`id`, `nom`, `adresse`,`ContratDebut`,`ContratFin`) VALUES ("+"'" + p.getId() + "', '" + p.getNom() + "', '"
                 + p.getAdresse() + "','"+ p.getContratDebut()+"','" + p.getContratFin() + "')";
         executeQuery(SQL);
         return false;
     }
  
-    public boolean consulter(partenariat p) {
-    String SQL = "SELECT * FROM `partenariat` (`id`, `nom`, `adresse`,`ContratDebut`,`ContratFin`) VALUES ("+"'" + p.getId() + "', '" + p.getNom() + "', '"
-                + p.getAdresse() + "','"+ p.getContratDebut()+"','" + p.getContratFin() + "')";
-        executeQuery(SQL);
-        return true;
+   @Override
+    public ObservableList<Partenariat> consulter() {
+        ObservableList<Partenariat> utilisateurs = FXCollections.observableArrayList();
+        Connection conn = DBConnection.getInstance().getConnection();
+        String query = "SELECT * FROM `utilisateur`";
+        Statement st;
+        ResultSet rs;
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+           Partenariat partenariat;
+            while (rs.next()) {
+                Partenariat Partenariat = new Partenariat(rs.getInt("id"), rs.getString("nom"), rs.getString("adresse"), rs.getDate("ContratDebut"),
+                        rs.getDate("ContratFin"));
+                Partenariat.add(Partenariat);
+                
+            }
+           
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        ObservableList<Partenariat> Partenariat = null;
+        return Partenariat;
+        
     }
+    
 }
