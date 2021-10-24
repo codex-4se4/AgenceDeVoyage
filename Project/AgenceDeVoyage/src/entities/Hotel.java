741a4e0afad220e0a4381aab5fbe532cd6ce3771
Frija Jawher <jawher.frija@esprit.tn>
2021-10-24 22:32:27 +0200

    Update Hotel.java

--- Project/AgenceDeVoyage/src/entities/Hotel.java
+++ Project/AgenceDeVoyage/src/entities/Hotel.java
@@ -10,8 +10,45 @@ package entities;
  * @author Jawher-Louisa
  */
 public class Hotel extends Hebergement {
-    
+
     private int nbEtoiles;
-    
-    
+    private String formule;
+    private String typeChambre;
+
+    public Hotel(int nbEtoiles, String formule, String typeChambre, int id, int nbChambres, int nbPersonnes, boolean avecPiscine, boolean avecParking, boolean avecPlage, String adresse) {
+        this.nbEtoiles = nbEtoiles;
+        this.formule = formule;
+        this.typeChambre = typeChambre;
+    }
+
+    public Hotel(int nbEtoiles, String formule, String typeChambre) {
+        this.nbEtoiles = nbEtoiles;
+        this.formule = formule;
+        this.typeChambre = typeChambre;
+    }
+
+    public int getNbEtoiles() {
+        return nbEtoiles;
+    }
+
+    public void setNbEtoiles(int nbEtoiles) {
+        this.nbEtoiles = nbEtoiles;
+    }
+
+    public String getFormule() {
+        return formule;
+    }
+
+    public void setFormule(String formule) {
+        this.formule = formule;
+    }
+
+    public String getTypeChambre() {
+        return typeChambre;
+    }
+
+    public void setTypeChambre(String typeChambre) {
+        this.typeChambre = typeChambre;
+    }
+
 }
