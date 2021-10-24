ee7af1d4f8687d11de3e6cdd619139e5890f5a56
Frija Jawher <jawher.frija@esprit.tn>
2021-10-24 22:32:38 +0200

    Update HebergementService.java

--- Project/AgenceDeVoyage/src/services/HebergementService.java
+++ Project/AgenceDeVoyage/src/services/HebergementService.java
@@ -6,74 +6,64 @@
 package services;
 
 import entities.Hebergement;
-import services.IService;
 import database.DBConnection;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 
 /**
  *
  * @author Jawher-Louisa
  */
-public abstract class HebergementService implements IService<Hebergement>{
-    
+public class HebergementService implements IService<Hebergement> {
+
     @Override
-    public void ajouter(Hebergement h){
-        String SQL = "INSERT INTO `hebergement` (`nbChambres`, `nbPersonnes`, `avecPiscine`,`avecParking`,`avecPlage`,`adresse`) VALUES ("+"'" + h.getNbChambres() + "', '" + h.getNbPersonnes() + "', '" 
-                + h.isAvecPiscine() + "','"+ h.isAvecParking()+"','" + h.isAvecPlage() + "','" + h.getAdresse()+ "')";
+    public void ajouter(Hebergement h) {
+        String SQL = "INSERT INTO `hebergement` (`nbChambres`, `nbPersonnes`, `avecPiscine`,`avecParking`,avecPlage`,`adresse`) VALUES ("
+                + h.getNbChambres() + "," + h.getNbPersonnes() + "," + h.isAvecPiscine() + "," + h.isAvecParking() + "," + h.isAvecPlage() + ",'" + h.getAdresse() + "')";
         executeQuery(SQL);
     }
+
     @Override
     public boolean supprimer(Hebergement h) {
-    String SQL = "DELETE FROM 'hebergement' Where ID=" +h.getId() ;
-    executeQuery(SQL);
-        return true;
-    }
-    @Override
-    
-    
-    public boolean modifier(Hebergement h) {
-        String SQL = "UPDATE `hebergement` (`nbChambres`, `nbPersonnes`, `avecPiscine`,`avecParking`,`avecPlage`,`adresse`) VALUES ("+"'" + h.getNbChambres() + "', '" + h.getNbPersonnes() + "', '" 
-                + h.isAvecPiscine() + "','"+ h.isAvecParking()+"','" + h.isAvecPlage() + "','" + h.getAdresse()+ "')";
+        String SQL = "DELETE FROM `hebergement` WHERE id=" + h.getId();
         executeQuery(SQL);
         return true;
-    
-    } 
- 
+    }
+
+    @Override
+    public boolean modifier(Hebergement h) {
+        String SQL = "UPDATE `hebergement` SET nbChambres =" + h.getNbChambres() + ", nbPersonnes =" + h.getNbPersonnes()
+                + ",avecPiscine =" + h.isAvecPiscine() + ",avecParking =" + h.isAvecParking() + ",avecPlage=" + h.isAvecPlage()
+                + ",adresse='" + h.getAdresse() + "' WHERE id =" + h.getId();
+        executeQuery(SQL);
+        return true;
+    }
+
     @Override
     public ObservableList<Hebergement> consulter() {
-        ObservableList<Hebergement> hebergements =FXCollections.observableArrayList();
+        ObservableList<Hebergement> hebergements = FXCollections.observableArrayList();
         Connection conn = DBConnection.getInstance().getConnection();
-        String query ="SELECT * FROM hebergement";
+        String query = "SELECT * FROM `hebergement`";
         Statement st;
         ResultSet rs;
-        try{
-            st =conn.createStatement();
+        try {
+            st = conn.createStatement();
             rs = st.executeQuery(query);
             Hebergement hebergement;
-            while(rs.next()){
-                hebergement =new Hebergement(rs.getInt("nbChambres"),rs.getInt("nbPersonnes"),rs.getBoolean("avecPiscine"),
-                rs.getBoolean("avecParking"),rs.getBoolean("avecPlage"),rs.getString("adresse"));
+            while (rs.next()) {
+                hebergement = new Hebergement(rs.getInt("id"), rs.getInt("nbChambres"), rs.getInt("nbPersonnes"), rs.getBoolean("avecPiscine"),
+                        rs.getBoolean("avecParking"), rs.getBoolean("avecPlage"), rs.getString("adresse"));
                 hebergements.add(hebergement);
-                
+
             }
-           
-        }
-        catch(Exception ex){
+
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
         return hebergements;
-         
-         
-         }
+    }
 
-    
 }
