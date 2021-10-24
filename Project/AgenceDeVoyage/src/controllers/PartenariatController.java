8c8d22ccf2b8f1db19d1d711ce674e6527e9cc1b
Wiemifaoui <73914179+wiemifaoui@users.noreply.github.com>
2021-10-24 11:54:34 +0200

    Update PartenariatController.java

--- Project/AgenceDeVoyage/src/controllers/PartenariatController.java
+++ Project/AgenceDeVoyage/src/controllers/PartenariatController.java
@@ -5,10 +5,55 @@
  */
 package controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 /**
+ * FXML Controller class
  *
  * @author HP
  */
-public class PartenariatController {
+public class PartenariatController implements Initializable {
+
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField adress;
+    @FXML
+    private TextField idd;
+    @FXML
+    private Text nom;
+    @FXML
+    private Text adresse;
+    @FXML
+    private Text id;
+    @FXML
+    private Text ContratDebut;
+    @FXML
+    private Text ContratFin;
+    @FXML
+    private Button ajouter;
+    @FXML
+    private Button modifier;
+    @FXML
+    private Button supprimer;
+    @FXML
+    private DatePicker Debut;
+    @FXML
+    private DatePicker Fin;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
     
 }
