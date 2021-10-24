317c73038cc9639acd2ab9a685196226ed5873dc
Frija Jawher <jawher.frija@esprit.tn>
2021-10-24 22:32:34 +0200

    Update Appartement.java

--- Project/AgenceDeVoyage/src/entities/Appartement.java
+++ Project/AgenceDeVoyage/src/entities/Appartement.java
@@ -9,8 +9,35 @@ package entities;
  *
  * @author Jawher-Louisa
  */
-public class Appartement extends Hebergement{
+public class Appartement extends Hebergement {
     
     private int numeroEtage;
-    private boolean avecAscenceur;
+
+    private boolean avecAscenseur;
+
+    public Appartement() {
+    }
+
+    public Appartement(int numeroEtage, boolean avecAscenseur, int id, int nbChambres, int nbPersonnes, boolean avecPiscine, boolean avecParking, boolean avecPlage, String adresse) {
+        super(id, nbChambres, nbPersonnes, avecPiscine, avecParking, avecPlage, adresse);
+        this.numeroEtage = numeroEtage;
+        this.avecAscenseur = avecAscenseur;
+    }
+
+    public int getNumeroEtage() {
+        return numeroEtage;
+    }
+
+    public void setNumeroEtage(int numeroEtage) {
+        this.numeroEtage = numeroEtage;
+    }
+
+    public boolean isAvecAscenseur() {
+        return avecAscenseur;
+    }
+
+    public void setAvecAscenseur(boolean avecAscenseur) {
+        this.avecAscenseur = avecAscenseur;
+    }
+
 }
