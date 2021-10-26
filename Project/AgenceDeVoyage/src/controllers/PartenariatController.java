56ab6c6874e6a30e40a680664a78038046724028
Wiemifaoui <73914179+wiemifaoui@users.noreply.github.com>
2021-10-26 14:12:21 +0200

    Update PartenariatController.java

--- Project/AgenceDeVoyage/src/controllers/PartenariatController.java
+++ Project/AgenceDeVoyage/src/controllers/PartenariatController.java
@@ -6,13 +6,16 @@
 package controllers;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import services.Partenariatservice;
 
 /**
  * FXML Controller class
@@ -20,40 +23,63 @@ import javafx.scene.text.Text;
  * @author HP
  */
 public class PartenariatController implements Initializable {
+   @FXML
+    private Button ajouter;
+
+    @FXML
+    private Text ContratFin;
+
+    @FXML
+    private Button supprimer;
+
+    @FXML
+    private Text ContratDebut;
+
+    @FXML
+    private Button modifier;
 
     @FXML
     private TextField name;
-    @FXML
-    private TextField adress;
-    @FXML
-    private TextField idd;
-    @FXML
-    private Text nom;
+
     @FXML
     private Text adresse;
+
     @FXML
-    private Text id;
-    @FXML
-    private Text ContratDebut;
-    @FXML
-    private Text ContratFin;
-    @FXML
-    private Button ajouter;
-    @FXML
-    private Button modifier;
-    @FXML
-    private Button supprimer;
-    @FXML
-    private DatePicker Debut;
+    private TextField adress;
+
     @FXML
     private DatePicker Fin;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    private Text nom;
+
+    @FXML
+    private DatePicker Debut;
     
-}
+    
+
+/**private Partenariatservice partenariatservice
+     * @param event;*/
+
+    
+     public void getDate(ActionEvent event){
+        LocalDate myDate = Debut.getValue();
+                System.out.println(myDate.toString());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+           name.setStyle("-fx-text-inner-color: #a0a2ab ;");
+           adresse.setStyle("-fx-text-inner-color: #a0a2ab;");
+
+
+           
+           
+           
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    
+    }
+    
+   
+        
+    }
