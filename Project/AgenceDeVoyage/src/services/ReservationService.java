2175ba6e2c304d9680daad162ca0f1c56bfcad64
fatmaBe <73782362+fatmaBe@users.noreply.github.com>
2021-10-26 21:11:27 +0200

    Update ReservationService.java

--- Project/AgenceDeVoyage/src/services/ReservationService.java
+++ Project/AgenceDeVoyage/src/services/ReservationService.java
@@ -1,17 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package services;
 
-import entities.Reservation;
+import entities.Utilisateur;
 import database.DBConnection;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import entities.Reservation;
+import entities.Hebergement;
+import entities.Voiture;
+import services.UtilisateurService;
 
 /**
  *
@@ -19,54 +18,81 @@ import javafx.collections.ObservableList;
  */
 public class ReservationService implements IService<Reservation> {
 
+    
     @Override
     public void ajouter(Reservation r) {
-        String SQL = "INSERT INTO `reservation` (`idr`, `prix`, `date_debut`,`date_fin`,`type`) VALUES (" + "'" + r.getIdr()+ "', '" + r.getPrix()+ "', '"
-                + r.getDate_debut()+ "','" + r.getDate_fin()+ "','" + r.getType()+ "')";
+        String SQL = "INSERT INTO `reservation` (`type`, `date`, ``,`nom`, `prenom`,) VALUES (" + "'" + r.getType()+ "', '" + r.getDate()+ r.get()+ "', '" + r.getDate()+ "')";
+        executeQuery(SQL);
+    }
+    @Override
+    public void ajouter(Utilisateur u) {
+        String SQL = "INSERT INTO `utilisateur` (`nom`, `prenom`) VALUES (" + "'" + u.getNom() + "', '" + u.getPrenom()  + "')";
         executeQuery(SQL);
     }
 
     @Override
-    public boolean supprimer(Reservation r) {
-        String SQL = "DELETE FROM `utilisateur` WHERE id=" + r.getId();
-        executeQuery(SQL);
-        return true;
-
-    }
-
-    @Override
-    public boolean modifier(Reservation r) {
-        String SQL = "UPDATE `reservation` SET idr ='" + r.getIdr()+ "', prix ='" + r.getPrix()
-                + "',date_debut ='" + r.getDate_debut()+ "',date_fin ='" + r.getDate_fin()+ "',type='" + r.getType()
-                 + "' WHERE id =" + r.getId();
+    public boolean supprimer(Utilisateur u) {
+        String SQL = "DELETE FROM `utilisateur` WHERE id=" + u.getId();
         executeQuery(SQL);
         return true;
+
     }
 
     @Override
-    public ObservableList<Reservation > consulter() {
-        ObservableList<Reservation > Reservations = FXCollections.observableArrayList();
+    public boolean modifier(Utilisateur u) {
+        String SQL = "UPDATE `utilisateur` SET nom ='" + u.getNom() + "', prenom ='" + u.getPrenom()
+                + "',email ='" + u.getEmail() + "',cin ='" + u.getCin() + "',passeport='" + u.getPasseport()
+                + "',login='" + u.getLogin() + "',mdp='" + u.getMdp() + "' WHERE id =" + u.getId();
+        executeQuery(SQL);
+        return true;
+    }
+
+    @Override
+    public ObservableList<Utilisateur> consulter() {
+        ObservableList<Utilisateur> utilisateurs = FXCollections.observableArrayList();
         Connection conn = DBConnection.getInstance().getConnection();
-        String query = "SELECT * FROM `reservation`";
+        String query = "SELECT * FROM `utilisateur`";
         Statement st;
         ResultSet rs;
         try {
             st = conn.createStatement();
             rs = st.executeQuery(query);
-            Reservation  reservation ;
+            Utilisateur utilisateur;
             while (rs.next()) {
-               reservation = new Reservation(rs.getInt("id"), rs.getInt("idr"), rs.getDouble("prix"), rs.getDate("date_debut"),
-                        rs.getDate("date_fin"), rs.getString("type"));
-                Reservations.add(reservation);
+                utilisateur = new Utilisateur(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),
+                        rs.getString("cin"), rs.getString("passeport"), rs.getString("login"), rs.getString("mdp"));
+                utilisateurs.add(utilisateur);
 
             }
 
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        return Reservations;
+        return utilisateurs;
 
     }
 
-    
+    public Utilisateur consulterParEmail(String email) {
+        Connection conn = DBConnection.getInstance().getConnection();
+        String query = "SELECT * FROM `utilisateur` where email = '" + email + "'";
+        Statement st;
+        ResultSet rs;
+        Utilisateur utilisateur = null;
+        try {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+            if (!rs.next()) {
+                return null;
+            }
+            utilisateur = new Utilisateur(rs.getInt("id"), rs.getString("nom"), rs.getString("prenom"), rs.getString("email"),
+                    rs.getString("cin"), rs.getString("passeport"), rs.getString("login"), rs.getString("mdp"));
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return utilisateur;
+
+    }
+
 }
