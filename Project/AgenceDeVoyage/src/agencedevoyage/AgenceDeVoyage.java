d6c0d92bd0088d5a39722470df582d24b8d6a868
Wiemifaoui <73914179+wiemifaoui@users.noreply.github.com>
2021-10-26 14:11:10 +0200

    Update AgenceDeVoyage.java

--- Project/AgenceDeVoyage/src/agencedevoyage/AgenceDeVoyage.java
+++ Project/AgenceDeVoyage/src/agencedevoyage/AgenceDeVoyage.java
@@ -19,10 +19,13 @@ public class AgenceDeVoyage extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/Login.fxml"));
+
+         Parent root = FXMLLoader.load(getClass().getResource("/gui/partenariat.fxml"));
         Scene scene = new Scene(root,607,409);
+        
         stage.setScene(scene);
         stage.show();
+       
         stage.setResizable(false);
 
     }
