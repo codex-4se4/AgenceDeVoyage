e33aab7af1e7758f1d7564c5668ed0ad0468d42e
mohamed0504 <Mohamed.Bourguiba1@esprit.tn>
2021-10-28 16:29:14 +0100

    Update UtilisateursController.java

--- Project/AgenceDeVoyage/src/controllers/UtilisateursController.java
+++ Project/AgenceDeVoyage/src/controllers/UtilisateursController.java
@@ -7,19 +7,31 @@ package controllers;
 
 import com.jfoenix.controls.JFXButton;
 import entities.Utilisateur;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 import services.UtilisateurService;
 
 /**
@@ -73,6 +85,14 @@ public class UtilisateursController implements Initializable {
     private JFXButton supprimerButton;
 
     private Utilisateur selectedUtilisateur;
+    @FXML
+    private Circle circleProfil;
+    @FXML
+    private Circle circleUser;
+
+    private String pathPhoto;
+
+    private int currentUserId;
 
     /**
      * Initializes the controller class.
@@ -80,6 +100,7 @@ public class UtilisateursController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         utilisateurService = new UtilisateurService();
+        circleUser.setFill(new ImagePattern(new Image("/images/defaultUser.png")));
         showUtilisateurs();
     }
 
@@ -97,17 +118,17 @@ public class UtilisateursController implements Initializable {
 
     @FXML
     private void ajouterAction(ActionEvent event) {
-        try {
-            if (nom.getText().isEmpty() || prenom.getText().isEmpty() || email.getText().isEmpty()
-                    || cin.getText().isEmpty() || passeport.getText().isEmpty()
-                    || login.getText().isEmpty() || mdp.getText().isEmpty()) {
-                throw new Exception("Les champs à insérer ne peuvent pas être vides");
-            }
-            Utilisateur user = new Utilisateur(nom.getText(), prenom.getText(), email.getText(), cin.getText(), passeport.getText(), login.getText(), mdp.getText(), null);
-            utilisateurService.ajouter(user);
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (!validateAjoutData()) {
+            return;
         }
+        Utilisateur user = new Utilisateur(nom.getText(), prenom.getText(), email.getText(), cin.getText(), passeport.getText(), login.getText(), mdp.getText(), pathPhoto);
+        utilisateurService.ajouter(user);
+        clearFields();
+        showUtilisateurs();
+
+    }
+
+    private void clearFields() {
         nom.clear();
         prenom.clear();
         email.clear();
@@ -115,8 +136,7 @@ public class UtilisateursController implements Initializable {
         passeport.clear();
         login.clear();
         mdp.clear();
-        showUtilisateurs();
-
+        circleUser.setFill(new ImagePattern(new Image("/images/defaultUser.png")));
     }
 
     @FXML
@@ -128,25 +148,240 @@ public class UtilisateursController implements Initializable {
         email.setText(selectedUtilisateur.getEmail());
         login.setText(selectedUtilisateur.getLogin());
         passeport.setText(selectedUtilisateur.getPasseport());
+        pathPhoto = "file:///" + selectedUtilisateur.getPhoto().replace(" ", "/");
+        Image img = new Image(pathPhoto);
+        circleUser.setFill(new ImagePattern(img));
 
     }
 
     @FXML
     private void modifierAction(ActionEvent event) {
-        Utilisateur user = new Utilisateur(selectedUtilisateur.getId(), nom.getText(), prenom.getText(), email.getText(), cin.getText(), passeport.getText(), login.getText(), mdp.getText(), null);
-
+        if (selectedUtilisateur == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setHeaderText(null);
+            alert.setContentText("Vous devez sélectionner une ligne à modifier");
+            alert.showAndWait();
+            return;
+        }
+        if (!validateModificationData()) {
+            return;
+        }
+        Utilisateur user = new Utilisateur(selectedUtilisateur.getId(), nom.getText(), prenom.getText(), email.getText(), cin.getText(), passeport.getText(), login.getText(), mdp.getText(), pathPhoto);
         utilisateurService.modifier(user);
+        clearFields();
         showUtilisateurs();
 
     }
 
     @FXML
     private void supprimerAction(ActionEvent event) {
-        if (selectedUtilisateur != null) {
-            utilisateurService.supprimer(selectedUtilisateur);
-            showUtilisateurs();
+        if (selectedUtilisateur == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setHeaderText(null);
+            alert.setContentText("Vous devez sélectionner une ligne à supprimer");
+            alert.showAndWait();
+            return;
+        }
+        utilisateurService.supprimer(selectedUtilisateur);
+        clearFields();
+        showUtilisateurs();
+
+    }
+
+    private boolean isValidEmail(String s) {
+        Pattern p = Pattern.compile("^[a-zA-Z-]+@[a-zA-Z-]+\\.[a-zA-Z]{2,6}$");
+        Matcher m = p.matcher(s);
+        return m.find() && m.group().equals(s);
+    }
+
+    private boolean isValidTextField(String s) {
+        Pattern p = Pattern.compile("[a-zA-Z]+");
+        Matcher m = p.matcher(s);
+        return m.find() && m.group().equals(s);
+    }
+
+    private boolean isValidNumber(String s) {
+        Pattern p = Pattern.compile("[0-9]+");
+        Matcher m = p.matcher(s);
+        return m.find() && m.group().equals(s);
+    }
+
+    private boolean isValidAlphaNumeric(String s) {
+        Pattern p = Pattern.compile("[A-Za-z0-9]+");
+        Matcher m = p.matcher(s);
+        return m.find() && m.group().equals(s);
+    }
+
+    private boolean isValidLogin(String s) {
+        Pattern p = Pattern.compile("^[a-zA-Z][A-Za-z0-9]+");
+        Matcher m = p.matcher(s);
+        return m.find() && m.group().equals(s);
+    }
+
+    private boolean isUniqueLogin(String s) {
+        return utilisateurService.getUserByLogin(login.getText()) == null;
+
+    }
+
+    private boolean isUniqueEmail(String s) {
+        return utilisateurService.consulterParEmail(email.getText()) == null;
+
+    }
+
+    private boolean validateAjoutData() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erreur de saisie");
+        alert.setHeaderText(null);
+        StringBuilder sb = new StringBuilder();
+        if (nom.getText().isEmpty()) {
+            sb.append("Votre nom est vide " + "\n");
+        } else if (!isValidTextField(nom.getText())) {
+            sb.append("Votre nom n'est pas valide " + "\n");
+        }
+
+        if (prenom.getText().isEmpty()) {
+            sb.append("Votre prenom est vide " + "\n");
+        } else if (!isValidTextField(prenom.getText())) {
+            sb.append("Votre prenom n'est pas valide " + "\n");
+        }
+
+        if (email.getText().isEmpty()) {
+            sb.append("Votre email est vide " + "\n");
+        } else if (!isValidEmail(email.getText())) {
+            sb.append("Votre email n'est pas valide " + "\n");
+        } else if (!isUniqueEmail(email.getText())) {
+            sb.append("Vous avez déjà un compte avec cet email " + "\n");
+        }
+
+        if (cin.getText().isEmpty()) {
+            sb.append("Votre cin est vide " + "\n");
+        } else if (!isValidNumber(cin.getText()) || cin.getText().length() != 8) {
+            sb.append("Votre cin n'est pas valide " + "\n");
+        }
+
+        if (passeport.getText().isEmpty()) {
+            sb.append("Votre passeport est vide " + "\n");
+        } else if (!isValidAlphaNumeric(passeport.getText()) || passeport.getText().length() != 6) {
+            sb.append("Votre passeport n'est pas valide " + "\n");
+        }
+
+        if (login.getText().isEmpty()) {
+            sb.append("Votre login est vide " + "\n");
+        } else if (!isValidLogin(login.getText()) || login.getText().length() < 4) {
+            sb.append("Votre login doit contenir des chiffres et/ou des lettres et doit être supérieur à 4 " + "\n");
+        } else if (!isUniqueLogin(login.getText())) {
+            sb.append("Votre login existe déjà " + "\n");
 
         }
+
+        if (mdp.getText().isEmpty()) {
+            sb.append("Votre mot de passe est vide " + "\n");
+        } else if (!isValidAlphaNumeric(mdp.getText()) || mdp.getText().length() < 4) {
+            sb.append("Votre mot de passe doit contenir des chiffres et des lettres et doit être supérieur à 4 " + "\n");
+        }
+
+        if (pathPhoto == null) {
+            sb.append("Votre photo de profil est vide " + "\n");
+        }
+
+        if (sb.length() != 0) {
+            alert.setContentText(sb.toString());
+            alert.showAndWait();
+            return false;
+        }
+
+        return true;
+
+    }
+
+    public void setUserInformation(Utilisateur u) throws FileNotFoundException {
+        currentUserId = u.getId();
+        nomComplet.setText(u.getPrenom() + " " + u.getNom());
+        pathPhoto = "file:///" + u.getPhoto().replace(" ", "/");
+        Image img = new Image(pathPhoto);
+        circleProfil.setFill(new ImagePattern(img));
+    }
+
+    @FXML
+    private void browseAction(MouseEvent event) throws IOException {
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("JPG", "*.jpg"), new FileChooser.ExtensionFilter("PNG", "*.png"),
+                new FileChooser.ExtensionFilter("JPEG", "*.jpeg"));
+        File file = fc.showOpenDialog(null);
+        if (file != null) {
+            pathPhoto = file.getAbsolutePath();
+            Image image = SwingFXUtils.toFXImage(ImageIO.read(file), null);
+            circleUser.setFill(new ImagePattern(image));
+
+        }
+    }
+
+    private boolean validateModificationData() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erreur de saisie");
+        alert.setHeaderText(null);
+        StringBuilder sb = new StringBuilder();
+        if (nom.getText().isEmpty()) {
+            sb.append("Votre nom est vide " + "\n");
+        } else if (!isValidTextField(nom.getText())) {
+            sb.append("Votre nom n'est pas valide " + "\n");
+        }
+
+        if (prenom.getText().isEmpty()) {
+            sb.append("Votre prenom est vide " + "\n");
+        } else if (!isValidTextField(prenom.getText())) {
+            sb.append("Votre prenom n'est pas valide " + "\n");
+        }
+
+        if (email.getText().isEmpty()) {
+            sb.append("Votre email est vide " + "\n");
+        } else if (!isValidEmail(email.getText())) {
+            sb.append("Votre email n'est pas valide " + "\n");
+        } else if (!(isUniqueEmail(email.getText()) || utilisateurService.consulterParEmail(email.getText()).getId() == currentUserId)) {
+            sb.append("Vous avez déjà un compte avec cet email " + "\n");
+        }
+
+        if (cin.getText().isEmpty()) {
+            sb.append("Votre cin est vide " + "\n");
+        } else if (!isValidNumber(cin.getText()) || cin.getText().length() != 8) {
+            sb.append("Votre cin n'est pas valide " + "\n");
+        }
+
+        if (passeport.getText().isEmpty()) {
+            sb.append("Votre passeport est vide " + "\n");
+        } else if (!isValidAlphaNumeric(passeport.getText()) || passeport.getText().length() != 6) {
+            sb.append("Votre passeport n'est pas valide " + "\n");
+        }
+
+        if (login.getText().isEmpty()) {
+            sb.append("Votre login est vide " + "\n");
+        } else if (!isValidLogin(login.getText()) || login.getText().length() < 4) {
+            sb.append("Votre login doit contenir des chiffres et des lettres et doit être supérieur à 4 " + "\n");
+        } else if (!(isUniqueLogin(login.getText()) || utilisateurService.getUserByLogin(login.getText()).getId() == currentUserId)) {
+            sb.append("Votre login existe déjà " + "\n");
+
+        }
+
+        if (mdp.getText().isEmpty()) {
+            sb.append("Votre mot de passe est vide " + "\n");
+        } else if (!isValidAlphaNumeric(mdp.getText()) || mdp.getText().length() < 4) {
+            sb.append("Votre mot de passe doit contenir des chiffres et/ou des lettres et doit être supérieur à 4 " + "\n");
+        }
+
+        if (pathPhoto == null) {
+            sb.append("Votre photo de profil est vide " + "\n");
+        }
+
+        if (sb.length() != 0) {
+            alert.setContentText(sb.toString());
+            alert.showAndWait();
+            return false;
+        }
+
+        return true;
+
     }
 
 }
