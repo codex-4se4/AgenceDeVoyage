307ae21bab24f49d84b34f5b23193f7e496779df
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-11-02 20:49:04 +0100

    Update UtilisateurController.php

--- Project/PHP Project/agenceDeVoyage/src/Controller/UtilisateurController.php
+++ Project/PHP Project/agenceDeVoyage/src/Controller/UtilisateurController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\UtilisateurFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +17,16 @@ class UtilisateurController extends AbstractController
     {
         return $this->render('utilisateur/index.html.twig', [
             'controller_name' => 'UtilisateurController',
+        ]);
+    }
+
+    public function ajouterUtilisateur(Request $request): Response
+    {
+        $form = $this->createForm(UtilisateurFormType::class);
+
+        return $this->render("utilisateur/utilisateur-form.html.twig",[
+            "form_title" => "Ajouter utilisateur",
+            "form_utilisateur" => $form->createView(),
         ]);
     }
 }
