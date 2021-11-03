23237521078671a9c46aba3ca120bc3b186dd5db
mohamed0504 <mohamed.bourguiba1@esprit.tn>
2021-11-03 21:52:51 +0100

    Update UtilisateurFormType.php

--- Project/PHP Project/agenceDeVoyage/src/Form/UtilisateurFormType.php
+++ Project/PHP Project/agenceDeVoyage/src/Form/UtilisateurFormType.php
@@ -4,7 +4,12 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +18,13 @@ class UtilisateurFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('email')
+            ->add('nom',TextType::class)
+            ->add('prenom',TextType::class)
+            ->add('email',EmailType::class)
             ->add('cin')
             ->add('passeport')
             ->add('login')
-            ->add('mdp')
+            ->add('mdp',PasswordType::class)
             ->add('photo')
             ->add('role')
             ->add('submit', SubmitType::class)
